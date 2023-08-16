c73b4ad99a17eb70d8af9e8d696ff2f7fdb31d02
Kristaps <strazds@mail.ru>
2023-08-16 15:25:58 +0300

    [dev_feature-create-main-view] Created DeviceTable view and its layout

---
+++ src/components/Layout.vue
@@ -0,0 +1,57 @@
+<template>
+  <Layout :siteName="siteName">
+    <LayoutHeader>
+      <div class="logo">{{ siteName }}</div>
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        v-model:selectedKeys="selectedKeys"
+        :style="{ lineHeight: '64px' }"
+      >
+        <MenuItem key="1">Table</MenuItem>
+      </Menu>
+    </LayoutHeader>
+    <LayoutContent style="padding: 0 50px">
+      <slot> </slot>
+    </LayoutContent>
+  </Layout>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import {
+  Layout,
+  LayoutHeader,
+  LayoutContent,
+  Menu,
+  MenuItem,
+} from 'ant-design-vue';
+
+defineProps({
+  siteName: String,
+});
+
+const selectedKeys = ref(['1']);
+</script>
+
+<style scoped>
+.site-layout-content {
+  min-height: 280px;
+  padding: 24px;
+  background: #fff;
+}
+.ant-layout-header .logo {
+  color: #fff;
+  font-size: 22px;
+  text-transform: uppercase;
+  margin: 0 24px 0 0;
+  width: auto;
+  float: left;
+  height: auto;
+  line-height: 64px;
+}
+
+[data-theme='dark'] .site-layout-content {
+  background: #141414;
+}
+</style>
