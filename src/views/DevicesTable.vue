d3d1da1d54498a175c183ec9c620d2d079cfc708
Kristaps <strazds@mail.ru>
2023-08-17 02:00:26 +0300

    [dev_feature-fill-table] Returned data and filled out table with devices

--- src/views/DevicesTable.vue
+++ src/views/DevicesTable.vue
@@ -8,48 +8,89 @@
       />
       <Button type="primary" @click="getDevicesList">Submit</Button>
     </InputGroup>
-    <Table :columns="columns" />
+
+    <Table :dataSource="tableContent" :columns="columns" :loading="isLoading">
+    </Table>
   </Layout>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, computed, watchEffect } from 'vue';
 import Layout from '../components/Layout.vue';
-import { Table, InputGroup, Input, Button } from 'ant-design-vue';
+import { Table, InputGroup, Input, Button, message } from 'ant-design-vue';
 import { useDevices } from '../composables/useDevices';
 
-const { getDevices } = useDevices();
-
-const columns = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-  },
-  {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age',
-  },
-  {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address',
-  },
-];
-
-const apiKey = ref('');
+const { getDevices, isLoading, deviceList } = useDevices();
 
 defineProps({
   pageTitle: String,
 });
 
+const apiKey = ref('');
+
+const columns = [
+  {
+    title: 'Display Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Telemetry',
+    dataIndex: 'telemetryTypes',
+    key: 'telemetryTypes',
+  },
+  {
+    title: 'Units',
+    dataIndex: 'units',
+    key: 'units',
+  },
+  {
+    title: 'Zones',
+    dataIndex: 'zones',
+    key: 'zones',
+  },
+  {
+    title: 'Connectivity',
+    dataIndex: 'status',
+    key: 'status',
+  },
+  {
+    title: 'Data Streaming',
+    dataIndex: 'isDisabled',
+    key: 'isDisabled',
+  },
+];
+
+const devices = computed(() => deviceList?.value);
+
+// Store all the devices table data in one array.
+const tableContent = computed(() => {
+  return devices?.value?.data?.data?.devices?.map((value) => {
+    return {
+      name: value?.displayName,
+      telemetryTypes: value?.telemetryTypes[0]?.name,
+      units: value?.units[0]?.name,
+      zones: value?.zones[0]?.name,
+      status: value?.status ? 'On' : 'Off',
+      isDisabled: value?.isDisabled ? 'On' : 'Off',
+    };
+  });
+});
+
+// Passing payload and API Key to the getDevices method to execute POST request
 const getDevicesList = () => {
+  // Hardcoded payload based on API doc.
   const payload = {
     orderTypes: 3,
     page: 1,
     pageSize: 100,
   };
+
+  if (!apiKey.value.length) {
+    message.warning('Please enter API Key');
+
+    return;
+  }
 
   getDevices(payload, apiKey.value);
 };
